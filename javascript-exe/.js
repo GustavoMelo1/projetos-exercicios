63ba55e7cce655ef7e61f86926f7e542492cd008
GustavoMelo1 <gustavomelo.oliveira@hotmail.com>
2025-08-05 19:33:11 -0300

    primeiro js

---
+++ javascript-exe/.js
@@ -0,0 +1,17 @@
+console.log("Olá, mundo!");
+
+let nomes = ['maria', 'jose', 'joao']
+
+console.log(nomes)
+
+for(let i = 0; i < nomes.length; i++)
+    console.log("O nome e: " + nomes [i])
+
+let saudacoes = 0;
+
+while (saudacoes < nomes.length) {
+    console.log("o nome e: " + nomes[saudacoes]);
+    saudacoes++;
+}
+
+
